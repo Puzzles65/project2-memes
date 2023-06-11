d2052e976cdfa90338a6dab4f3543342c67a4674
Puzzles65 <chitrungnguyen12@gmail.com>
2023-06-11 18:03:30 +0100

    authadded

--- routes/memes.js
+++ routes/memes.js
@@ -2,23 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 const memesCtrl = require('../controllers/memes');
+const ensureLoggedIn = require('../config/ensureLoggedIn');
 //const meme = require('../models/meme');
 // getting all memes
 router.get('/', memesCtrl.index);
 // posting new meme
-router.post('/', memesCtrl.create)
+router.post('/', ensureLoggedIn, memesCtrl.create)
 // Getting the "New Meme" page
-router.get('/new', memesCtrl.new);
+router.get('/new', ensureLoggedIn, memesCtrl.new);
 // getting a specific meme
 router.get('/:id', memesCtrl.show)
 
 // updating meme 
-router.post('/:id/updateForm', memesCtrl.updateForm);
+router.post('/:id/updateForm', ensureLoggedIn, memesCtrl.updateForm);
 
-router.post('/:id/update', memesCtrl.update);
+router.post('/:id/update', ensureLoggedIn, memesCtrl.update);
 //router.put('/:id/update', memesCtrl.update)
 // deleting 
-router.post('/:id/delete', memesCtrl.remove)
+router.post('/:id/delete', ensureLoggedIn, memesCtrl.remove)
 
 
 module.exports = router;
